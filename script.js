f71a110deccacc374fcf06db03bb1c3b558fcdeb
McKenna Roessler <roesslerm135@gmail.com>
2024-11-12 16:20:48 -0600

    Write computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("Hello world");
+
+function getComputerChoice() 
+{
+    let computerChoice = "";
+    let randomNum = Math.random;
+
+    if (randomNum <= 0.33)
+    {
+        computerChoice = "rock";
+    }
+    else if (randomNum >= 0.67)
+    {
+        computerChoice = "paper";
+    }
+    else
+    {
+        computerChoice = "scissors";
+    }
+    
+    return computerChoice;
+}
+
+function getHumanChoice()
+{
+
+}
